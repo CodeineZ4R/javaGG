740149120a0f57d59c324b56be4189842da3dd1f
CodeineZ4R <codeine.z4r@gmail.com>
2017-09-22 06:15:53 +0300

    test array

--- src/com/gg/array/Array.java
+++ src/com/gg/array/Array.java
@@ -7,17 +7,55 @@ public class Array {
 
     public static int[] arrayNum = new int[20];
 
+    public static void main(String[] args) {
+        toFillArray();
+        goBack();
+        transferLast();
+        transferFirst();
+    }
+
     public static void toFillArray() {
         for (int i = 0; i < arrayNum.length; i++) {
             arrayNum[i] = i;
-            System.out.print(arrayNum[i] + " ");
+            System.out.printf("%d ", arrayNum[i]);
         }
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        toFillArray();
+    public static void goBack() {
+        for (int i = arrayNum.length - 1; i >= 0; i--) {
+            System.out.printf("%d ", arrayNum[i]);
+        }
+        System.out.println();
     }
 
+    public static void transferFirst() {
+        int tmp ;
+
+        for (int i = 0; i <arrayNum.length-1 ; i++) {
+            tmp = arrayNum[i];
+            arrayNum[i] = arrayNum[i+1];
+            arrayNum[i+1] =tmp;
+        }
+
+        for (int i : arrayNum) {
+            System.out.printf("%d ",arrayNum[i]);
+        }
+        System.out.println();
+    }
+
+    public static void transferLast() {
+        int tmp;
+        for (int i = arrayNum.length - 1; i > 0; i--) {
+            tmp = arrayNum[i];
+            arrayNum[i] = arrayNum[i-1];
+            arrayNum[i-1] = tmp;
+        }
+
+        for (int i : arrayNum) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+    }
 
 }
---
+++ src/com/gg/binarysearch/Binarysearch.java
@@ -0,0 +1,10 @@
+package com.gg.binarysearch;
+
+/**
+ * Created by admin on 22.09.2017.
+ */
+public class Binarysearch {
+    public static void main(String[] args) {
+        
+    }
+}
